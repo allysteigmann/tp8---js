7d33528217f95ee34f2b67a94dbb1b6455746239
allysteigmann <alysonsteigmann@gmail.com>
2022-11-10 17:58:14 +0000

    added patterns and basic clicks

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* JavaScript code for memory game TP8-JS */
+
+window.onload = function() {
+  
+  cardList = document.querySelectorAll(".card"); //collection of cards 
+  
+  cardCount = cardList.length; //how many cards are on the table
+  
+  for (c = 0; c < cardCount; c++) {
+    cardList[c].onclick = function() {
+      this.classList.toggle("clicked");
+    }
+  }
+  
+}
